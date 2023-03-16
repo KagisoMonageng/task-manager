5dfddec968b3e5df24a439788cdef74183de1cd6
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-03-16 22:53:57 +0200

    database readings

--- Backend/routes/tasks.route.js
+++ Backend/routes/tasks.route.js
@@ -6,7 +6,7 @@ const task = require("../controllers/task");
 
 //Authorization
 router.get("/:user_id", task.getMyTasks);
-router.delete("/:task_id", task.deleteTask);
+//router.delete("/:task_id", task.deleteTask);
 router.get("/in-progress/:user_id", task.getMyInProgress);
 
 
